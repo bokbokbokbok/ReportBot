1400953b0136f14df246c5093cccf292715f7c92
Vlad <124751524+fedyafedko@users.noreply.github.com>
2024-08-21 11:17:54 +0300

    add filter users (#4)

--- ReportBot/ReportBot.Services/Profiles/UserProfile.cs
+++ ReportBot/ReportBot.Services/Profiles/UserProfile.cs
@@ -9,7 +9,9 @@ public class UserProfile : Profile
 {
     public UserProfile()
     {
-        CreateMap<User, WorksnapsUserDTO>();
+        CreateMap<User, WorksnapsUserDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.WorksnapsId))
+            .ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.Username));
         CreateMap<User, UserDTO>();
     }
 }
